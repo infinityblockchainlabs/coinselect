2d2e89f97e2b2ee6867864a1013f32a130063253
khoitd-ibl <khoitd@cardano-labo.com>
2017-11-03 09:35:57 +0700

    update address

--- sendMax.js
+++ sendMax.js
@@ -4,6 +4,10 @@ module.exports = function sendMax (utxos, outputs, feeRate) {
   if (!isFinite(utils.uintOrNaN(feeRate))) return {}
   var sumInput = utils.sumOrNaN(utxos);
   outputs[0].value = sumInput;
+  if (!outputs[0].address) {
+    // add fake address to estimate fee
+    outputs[0].address = '0000000000000000000000000000000000';
+  }
   var bytesAccum = utils.transactionBytes(utxos, outputs)
   var fee = feeRate * bytesAccum
   var outputValue = sumInput - fee;
